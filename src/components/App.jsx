0ba2d1e8c5dbdf2f0f6fe3370acf1ea674f38e37
Aleksandr Tatarenko <aleksandrcrimea96@gmail.com>
2022-10-29 12:23:17 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import api from '../services/api';
 import { Container } from 'components/App.styled';
 import { Searchbar } from 'components/Searchbar/Searchbar';
@@ -7,77 +7,66 @@ import { LoadMore } from 'components/LoadMore/LoadMore';
 import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    error: null,
-    modalImageURL: null,
-    isOpen: false
-  }
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalImageURL, setModalImageURL] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, page } = this.state;
-    if (searchQuery !== prevState.searchQuery || page !== prevState.page) {
-      try {
-        this.setState({ isLoading: true });
-        const images = await api.fetchImages(searchQuery, page);
-        if (prevState.searchQuery === this.state.searchQuery) {
-          this.setState(prevState => {
-          return { images: [...prevState.images, ...images] }
-        });
-        } else {
-          this.setState({ images: images });
+  useEffect(() => {
+    async function fetchImages(searchQuery, page) {
+        try {
+          setIsLoading(true);
+          const images = await api.fetchImages(searchQuery, page);
+          if (page === 1) {
+            setImages(images);
+          } else {
+            setImages((prev) => [...prev, ...images]);
+          };
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
         }
-        
-      } catch (error) {
-        this.setState({error: "Не получилось загрузить изображения"})
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-    return null;
+    };
+    if (searchQuery !== null) {
+      fetchImages(searchQuery, page);
+    } 
+  }, [searchQuery, page]);
+  
+  const onSubmit = (searchQuery) => {
+    setSearchQuery(searchQuery.toString());
+    setPage(1);
+  };
+
+  const onButtonClick = () => {
+    setPage(prev => {
+      return prev + 1;
+    });
+  };
+
+  const onItemClick = (id) => {
+    const modalImage = images.find(image => image.id === id);
+    setModalImageURL(modalImage.largeImageURL);
+    setIsOpen(true);
   }
 
-  onSubmit = (searchQuery) => {
-    this.setState({
-      searchQuery: [searchQuery.toString()],
-      page: 1
-    })
-  }
-
-  onButtonClick = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 }
-    })
-  }
-
-  onItemClick = (id) => {
-    const modalImage = this.state.images.find(image => image.id === id);
-    this.setState({
-      modalImageURL: modalImage.largeImageURL,
-      isOpen: true
-    })
-  }
-
-  onOverlayClick = (e) => {
+  const onOverlayClick = (e) => {
     const overlay = document.getElementById('Overlay');
     if (e.target===overlay) {
-      this.setState({ isOpen: false });
+      setIsOpen(false);
     }
   }
 
-  render() {
-    const { images, isLoading, isOpen, modalImageURL} = this.state;
-    return (
+  return (
       <Container>
-        <Searchbar onSubmit={this.onSubmit} />
-        {isOpen ? <Modal onClick={this.onOverlayClick} largeImageUrl={modalImageURL} />:null}
+        <Searchbar onSubmit={onSubmit} />
+        {isOpen ? <Modal onClick={onOverlayClick} largeImageUrl={modalImageURL} />:null}
         {isLoading ? <Loader /> : null}
-        {images.length > 0 && <ImageGallery images={images} onClick={this.onItemClick}/>}
-        {images.length >= 12 && <LoadMore onClick={this.onButtonClick} />}
+        {images.length > 0 && <ImageGallery images={images} onClick={onItemClick}/>}
+        {images.length >= 12 && <LoadMore onClick={onButtonClick} />}
     </Container>
-  );}
-};
+  );
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,41 +1,73 @@
 import PropTypes from 'prop-types';
-import { Component } from "react";
+import { useState} from "react";
 import { Header, Form, Button, Input, Span } from 'components/Searchbar/Searchbar.styled';
 
-export class Searchbar extends Component {
-    state = {
-        searchQuery: ''
-    }
-    handleSubmit = (e) => {
+export const Searchbar = ({onSubmit}) => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSubmit = (e) => {
         e.preventDefault()
-        if (this.state.searchQuery === '') { 
+        if (searchQuery === '') {
             alert('Заполните строку поиска');
             return;
         }
-        this.props.onSubmit(this.state.searchQuery);
-    }
-    handleChange = (e) => {
-        this.setState({ searchQuery: [e.target.value.toLowerCase()] });
-    }
-    render() {
-        return <Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Button type="submit">
-            <Span>Search</Span>
-          </Button>
+        onSubmit(searchQuery);
+    };
 
-            <Input
-            onChange={this.handleChange}
-            value={this.state.searchQuery}        
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value.toLowerCase());
+    };
+
+    return (<Header>
+         <Form onSubmit={handleSubmit}>
+           <Button type="submit">
+             <Span>Search</Span>
+           </Button>
+             <Input
+            onChange={handleChange}
+            value={searchQuery}        
             type="text"
             autocomplete="off"
             autoFocus
             placeholder="Search images and photos"
           />
         </Form>
-        </Header>
-    }
+        </Header>)
 }
+// export class Searchbar extends Component {
+//     state = {
+//         searchQuery: ''
+//     }
+//     handleSubmit = (e) => {
+//         e.preventDefault()
+//         if (this.state.searchQuery === '') { 
+//             alert('Заполните строку поиска');
+//             return;
+//         }
+//         this.props.onSubmit(this.state.searchQuery);
+//     }
+//     handleChange = (e) => {
+//         this.setState({ searchQuery: [e.target.value.toLowerCase()] });
+//     }
+//     render() {
+//         return <Header>
+//         <Form onSubmit={this.handleSubmit}>
+//           <Button type="submit">
+//             <Span>Search</Span>
+//           </Button>
+
+//             <Input
+//             onChange={this.handleChange}
+//             value={this.state.searchQuery}        
+//             type="text"
+//             autocomplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </Form>
+//         </Header>
+//     }
+// }
 
 Searchbar.propTypes = {
     onSubmit: PropTypes.func.isRequired
